95fa072ffb02d2e8f0238ae98e2c84ed7645bce1
ThomK1429 <sndbytgkv1@verizon.net>
2016-10-02 12:00:43 -0400

    Create seedsLogv1.sql

---
+++ seedsLogv1.sql
@@ -0,0 +1,33 @@
+INSERT INTO bglog_tbl
+	(userId, userName, bgMgdl, readingType, notes ) 
+VALUES
+
+		(05, 'Amanda', 80,  'a', 'asdfdfasdf'),
+		(05, 'Amanda', 180, 'b', 'qwertyqwerty'),
+		(05, 'Amanda', 120,  'c', 'dovarkdovark'),
+		(05, 'Amanda', 150,  'd', 'uiopiopuiopuiop'), 
+
+		(10, 'Julian', 81,  'a', 'asdfdfasdf'),
+		(10, 'Julian', 181, 'b', 'qwertyqwerty'),
+		(10, 'Julian', 121, 'c', 'dovarkdovark'),
+		(10, 'Julian', 151, 'd', 'uiopiopuiopuiop'), 
+
+		(15, 'Rangy', 82,   'a', 'asdfdfasdf'),
+		(15, 'Rangy', 182,  'b', 'qwertyqwerty'),
+		(15, 'Rangy', 122,  'c', 'dovarkdovark'),
+		(15, 'Rangy', 152,  'd', 'uiopiopuiopuiop'), 
+
+		(20, 'Stephen', 83,  'a', 'asdfdfasdf'),
+		(20, 'Stephen', 183, 'b', 'qwertyqwerty'),
+		(20, 'Stephen', 123, 'c', 'dovarkdovark'),
+		(20, 'Stephen', 153, 'd', 'uiopiopuiopuiop'),
+
+		(25, 'Tom', 84,  'a', 'asdfdfasdf'),
+		(25, 'Tom', 184, 'b', 'qwertyqwerty'),
+		(25, 'Tom', 124, 'c', 'dovarkdovark'),
+		(25, 'Tom', 154, 'd', 'uiopiopuiopuiop');
+
+	
+SHOW DATABASES LIKE 'geeB_db';
+SHOW COLUMNS from bglog_tbl;
+SELECT * from bglog_tbl;
