5cda81f233bd9796649f965305c547b5d878285b
Rangy Abines <rangyabines11@gmail.com>
2016-10-04 19:19:31 -0400

    updated graphJS file

--- app/components/Children/Graph.js
+++ app/components/Children/Graph.js
@@ -1,32 +1,29 @@
 // Include React
 var React = require('react');
+var LineChart = require("react-chartjs").Line;
 
 var Graph = React.createClass({
 
   render: function(){
 
-    return(
-      <div className="container">
-
-        <div className="col-lg-12">
-          <div className="panel panel-danger">
-            <div className="panel-heading">
-              <h3 className="panel-title">Graph</h3>
-            </div>
-            <div className="panel-body">
-              Panel content
-            </div>
-          </div>
-        </div>
-
-      </div>
-    )
+    return <LineChart data={chartData} options={chartOptions}/>
+    // return(
+    //   <div className="container">
+    //
+    //     <div className="col-lg-12">
+    //       <div className="panel panel-danger">
+    //         <div className="panel-heading">
+    //           <h3 className="panel-title">Graph</h3>
+    //         </div>
+    //         <div className="panel-body">
+    //           Panel content
+    //         </div>
+    //       </div>
+    //     </div>
+    //
+    //   </div>
+    // )
   }
 });
 
 module.exports = Graph;
-
-
-
-
-
