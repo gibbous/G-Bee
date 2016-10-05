2d937acc1124052c77ab3ea8763ea4c06b21cdda
Stephen Doe <redcard1016@yahoo.com>
2016-10-05 18:14:59 -0400

    Basic form

--- app/components/Children/Log.js
+++ app/components/Children/Log.js
@@ -14,18 +14,71 @@ var Log = React.createClass({
               <h3 className="panel-title">Log</h3>
             </div>
             <div className="panel-body">
-              Panel content
-            </div>
-          </div>
-        </div>
+              function FieldGroup({ id, label, help, ...props }) {
+                return (
+                  <FormGroup controlId={id}>
+                    <ControlLabel>{label}</ControlLabel>
+                    <FormControl {...props} />
+                    {help && <HelpBlock>{help}</HelpBlock>}
+                  </FormGroup>
+                );
+              }
 
-      </div>
-    )
-  }
-});
+              const formInstance = (
+                <form>
+                  <FieldGroup
+                    id="formControlsDate"
+                    type="date"
+                    label="Date"
+                    placeholder="Enter Date"
+                  />
+                  <FieldGroup
+                    id="formControlsTime"
+                    type="time"
+                    label="Time"
+                    placeholder="Enter Time"
+                  />
+
+                  <FieldGroup
+                    id="formControlsReading"
+                    type="number"
+                    label="Blood Glucose Reading"
+                    placeholder=""
+                  />
+              
+                  <FormGroup controlId="formControlsSelect">
+                    <ControlLabel>Test Options</ControlLabel>
+                    <FormControl componentClass="select" placeholder="Choose Test Option">
+                      <option value="select">Out of Bed (fasting)</option>
+                      <option value="other">After Breakfast</option>
+                      <option value="other">After Lunch</option>
+                      <option value="other">After Dinner</option>
+                    </FormControl>
+                  </FormGroup>
+
+                  <FormGroup controlId="formControlsTextarea">
+                    <ControlLabel>Notes</ControlLabel>
+                    <FormControl componentClass="textarea" placeholder="textarea" />
+                  </FormGroup>
+
+                  <Button type="submit">
+                    Submit
+                  </Button>
+                </form>
+              );
+
+              ReactDOM.render(formInstance, mountNode);
+
+                          </div>
+                        </div>
+                      </div>
+
+                    </div>
+                  )
+                }
+              });
 
 module.exports = Log;
-
 
 
 
