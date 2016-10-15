ee31f88fe07aeb511d5445f100653f448c8b4815
Amanda Hoffman <amandah@alumni.rutgers.edu>
2016-10-14 22:43:31 -0400

    example badges and text changes

--- public/bundle.js
+++ public/bundle.js
@@ -27605,7 +27605,7 @@
 	Object.defineProperty(exports, "__esModule", {
 		value: true
 	});
-	exports.defaults = exports.Bubble = exports.Polar = exports.Radar = exports.HorizontalBar = exports.Bar = exports.Line = exports.Pie = exports.Doughnut = undefined;
+	exports.Polar = exports.Radar = exports.HorizontalBar = exports.Bar = exports.Line = exports.Pie = exports.Doughnut = exports.Bubble = undefined;
 
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -27957,8 +27957,6 @@
 
 		return Bubble;
 	}(_react2.default.Component);
-
-	var defaults = exports.defaults = _chart2.default.defaults;
 
 /***/ },
 /* 244 */
@@ -53177,7 +53175,7 @@
 		value: true
 	});
 
-	var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+	var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
 	var hasOwnProperty = Object.prototype.hasOwnProperty;
 
@@ -53203,15 +53201,10 @@
 		}
 
 		var keysA = Object.keys(objA);
-		var keysB = Object.keys(objB);
-		var allKeys = keysA.concat(keysB);
 
-		// Verify both objects have all the keys
-		for (var i = 0; i < allKeys.length; i++) {
-			if (!hasOwnProperty.call(objB, allKeys[i])) {
-				return false;
-			}
-			if (!hasOwnProperty.call(objA, allKeys[i])) {
+		// Test for A's keys different from B.
+		for (var i = 0; i < keysA.length; i++) {
+			if (!hasOwnProperty.call(objB, keysA[i])) {
 				return false;
 			}
 		}
